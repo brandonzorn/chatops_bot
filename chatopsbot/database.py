56aa7ce2c2011dd7c618f28e6c08ac6e378e6e66
bzorn <v.klimenko.2137@gmail.com>
2025-05-10 11:17:25 +0300

    feat: add postgres support

--- chatopsbot/database.py
+++ chatopsbot/database.py
@@ -3,11 +3,21 @@ from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config import DATABASE_NAME
+from config import (
+    DATABASE_NAME,
+    DATABASE_USER,
+    USE_SQLITE_DATABASE,
+    DATABASE_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+)
 from models import Base
 
-Path("sqlite").mkdir(exist_ok=True)
-DATABASE_URL = f"sqlite:///sqlite/{DATABASE_NAME}.db"
+if USE_SQLITE_DATABASE:
+    Path("sqlite").mkdir(exist_ok=True)
+    DATABASE_URL = f"sqlite:///sqlite/{DATABASE_NAME}.db"
+else:
+    DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)

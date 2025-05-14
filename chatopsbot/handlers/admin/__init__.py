9b6c4fea0cc5d667bb405f1798d62267ca57827d
bzorn <v.klimenko.2137@gmail.com>
2025-05-14 16:02:33 +0300

    feat: add change team step to change_conv

--- chatopsbot/handlers/admin/__init__.py
+++ chatopsbot/handlers/admin/__init__.py
@@ -4,7 +4,7 @@ from .add_team import add_team_conv
 from .review_requests import review_conv
 from .register_chat import register_chat_conv
 from .delete_employee import delete_conv
-from .change_role import change_conv
+from .change_employee import change_conv
 from .change_threshold import threshold_conv
 
 __all__ = [
--- chatopsbot/handlers/admin/change_employee.py
+++ chatopsbot/handlers/admin/change_employee.py
@@ -11,14 +11,14 @@ from telegram.ext import (
 )
 
 from database import session
-from models import Employee, Role
+from models import Employee, Role, Team
 from utils import require_admin
 
-CHOOSE_EMPLOYEE, CHOOSE_NEW_ROLE = range(2)
+CHOOSE_EMPLOYEE, CHOOSE_NEW_ROLE, CHOOSE_NEW_TEAM = range(3)
 
 
 @require_admin
-async def start_change_role(update: Update, _):
+async def start_change_employee(update: Update, _):
     employees = session.query(Employee).all()
     if not employees:
         await update.message.reply_text("Список сотрудников пуст.")
@@ -28,14 +28,14 @@ async def start_change_role(update: Update, _):
         [
             InlineKeyboardButton(
                 emp.full_name,
-                callback_data=f"changeRole_{emp.id}",
+                callback_data=f"changeEmp_{emp.id}",
             ),
         ]
         for emp in employees
     ]
     markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
-        "Выберите сотрудника для изменения роли:",
+        "Выберите сотрудника для изменения:",
         reply_markup=markup,
     )
     return CHOOSE_EMPLOYEE
@@ -53,7 +53,7 @@ async def choose_new_role(update: Update, context: ContextTypes.DEFAULT_TYPE):
         [
             InlineKeyboardButton(
                 role.role_name,
-                callback_data=f"newRole_{role.id}",
+                callback_data=f"newEmpRole_{role.id}",
             ),
         ]
         for role in roles
@@ -65,6 +65,30 @@ async def choose_new_role(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return CHOOSE_NEW_ROLE
 
 
+async def choose_new_team(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    new_role_id = int(query.data.split("_")[1])
+    context.user_data["new_role_id"] = new_role_id
+
+    teams = session.query(Team).all()
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                team.name,
+                callback_data=f"newEmpTeam_{team.id}",
+            ),
+        ]
+        for team in teams
+    ]
+    await query.edit_message_text(
+        "Выберите новую команду:",
+        reply_markup=InlineKeyboardMarkup(keyboard),
+    )
+    return CHOOSE_NEW_TEAM
+
+
 async def apply_role_update(
         update: Update,
         context: ContextTypes.DEFAULT_TYPE,
@@ -73,11 +97,13 @@ async def apply_role_update(
     await query.answer()
 
     emp_id = context.user_data["employee_id"]
-    new_role_id = int(query.data.split("_")[1])
+    new_role_id = context.user_data["new_role_id"]
+    new_team_id = int(query.data.split("_")[1])
 
     employee = session.get(Employee, emp_id)
 
     employee.role_id = new_role_id
+    employee.team_id = new_team_id
     session.commit()
 
     await query.edit_message_text(
@@ -93,13 +119,16 @@ async def cancel(update: Update, _):
 
 change_conv = ConversationHandler(
     allow_reentry=True,
-    entry_points=[CommandHandler("change_role", start_change_role)],
+    entry_points=[CommandHandler("change_employee", start_change_employee)],
     states={
         CHOOSE_EMPLOYEE: [
-            CallbackQueryHandler(choose_new_role, pattern="^changeRole_"),
+            CallbackQueryHandler(choose_new_role, pattern="^changeEmp_"),
         ],
         CHOOSE_NEW_ROLE: [
-            CallbackQueryHandler(apply_role_update, pattern="^newRole_"),
+            CallbackQueryHandler(choose_new_team, pattern="^newEmpRole_"),
+        ],
+        CHOOSE_NEW_TEAM: [
+            CallbackQueryHandler(apply_role_update, pattern="^newEmpTeam_"),
         ],
     },
     fallbacks=[CommandHandler("cancel", cancel)],
